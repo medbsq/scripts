4a51654d660aa756e6a71e87204410b4f5486a09
medbsq <med.bsq@gmail.com>
2020-06-21 13:37:22 +0100

    crowler and curl_it

---
+++ curl_it/crl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+function help(){
+echo "usage:"
+echo -e "\\t -l     curl list of urls"
+}
+
+function crl(){
+mkdir -p ./output
+echo -ne "curl  $1"\\r
+filename=$(echo "$1" | sha256sum |awk '{print $1}')
+curl -sL  $1 -o ./output/$filename &>/dev/null
+echo "$1     $filename" >> ./output/index
+}
+
+
+while getopts ":l:" OPTION
+do
+        case $OPTION in
+                l)
+                        export -f crl
+                        cat "$OPTARG" | xargs -n 1 -P 10 -I {} bash -c 'crl  "$@"' _ {}
+                        exit
+                        ;;
+                :)
+                        help
+                        exit 1
+                        ;;
+                \?)
+                        help
+                        exit 1
+                        ;;
+
+        esac
+done
+
+
+
